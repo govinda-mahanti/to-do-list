6c6f57b1cc9dcfc442694439873b430b915f663a
govinda-mahanti <gmahanti955@gmail.com>
2024-08-10 13:29:59 +0530

    add

--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 
-export const Connecction =()=>{
+const Connecction =()=>{
     mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
-}
+
+    mongoose.connection.on('connected',()=>{
+        console.log('Connected to MongoDB')
+    })
+    mongoose.connection.on('disconnected',()=>{
+        console.log('disConnected to MongoDB')
+    })
+    mongoose.connection.on('error',()=>{
+        console.log('erroe while the connection',error.message)
+    })
+} 
+
+export default Connecction
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv"
+import Connecction from "./database/db.js";
 
 dotenv.config({
     path: './.env'
 })
 
 const app=express()
+
+Connecction();
 
 app.listen(process.env.PORT, ()=>console.log(`your server is running in port ${process.env.PORT}`))
